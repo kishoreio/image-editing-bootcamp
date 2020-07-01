904e0eb79e943deeadf2da3a680e3b6d01f30df7
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-07-01 17:40:11 +0530

    Updates

---
+++ precache-manifest.53ef2e86d1a961ea99f891d3caa3a0ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a3b54319ec896aaead97d12126768e",
+    "url": "/image-editing-bootcamp/index.html"
+  },
+  {
+    "revision": "3c7cc387267f3797fb0d",
+    "url": "/image-editing-bootcamp/static/css/main.b4afa0d0.chunk.css"
+  },
+  {
+    "revision": "ce4dd25a5cf1239f9a3b",
+    "url": "/image-editing-bootcamp/static/js/2.ae8a7c57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-editing-bootcamp/static/js/2.ae8a7c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7cc387267f3797fb0d",
+    "url": "/image-editing-bootcamp/static/js/main.a9faca3b.chunk.js"
+  },
+  {
+    "revision": "b2257208cea15e57d79b",
+    "url": "/image-editing-bootcamp/static/js/runtime-main.4889bff6.js"
+  },
+  {
+    "revision": "de3fa3be63062d3c95b06881295ad558",
+    "url": "/image-editing-bootcamp/static/media/search.de3fa3be.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/image-editing-bootcamp/precache-manifest.53ef2e86d1a961ea99f891d3caa3a0ba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/image-editing-bootcamp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
